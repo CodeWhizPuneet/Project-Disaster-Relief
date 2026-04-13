a893de889470bfc8b9f8c8fa55284b381741b053
Puneet Shankar <puneetshankar2021@gmail.com>
2026-04-14 00:49:51 +0530

    Set global axios baseURL for deployed frontend

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
 import App from './App'
 import './index.css'
+
+const envBase = (import.meta.env.VITE_API_URL as string | undefined)?.trim()
+if (envBase) {
+  axios.defaults.baseURL = envBase.replace(/\/+$/, '')
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
